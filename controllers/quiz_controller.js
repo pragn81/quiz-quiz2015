b22b7f0b927b008f894b172233a0a6c34085b234
pragn81 <pragn81@gmail.com>
2015-08-03 18:27:01 +0100

    modelo y db

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,24 @@
-// GET /quizes/question
+var models = require ('../models/models.js');
+
+//get quizes/question
+exports.question = function (req, res){
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});	
+	})
+    
+};
+
+//get quizes/answer
+exports.answer = function(req, res) {
+	models.Quiz.findAll().success(function(quiz){
+	    if (req.query.respuesta.toLowerCase()== quiz[0].respuesta){
+	        res.render('quizes/answer', {respuesta: 'Correcto'});
+	    }
+	    else {
+	        res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	    }
+	})    
+};// GET /quizes/question
 exports.question = function(req, res) {
     res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
