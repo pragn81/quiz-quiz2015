cb150de37e5933135b7fd0381e3c9815a3062d9a
pragn81 <pragn81@gmail.com>
2015-08-03 19:11:00 +0100

    db sqlite

--- models/quiz.js
+++ models/quiz.js
@@ -1,4 +1,4 @@
 module.exports = function(sequelize,DataTypes){
     return sequelize.define('Quiz', {
         pregunta: DataTypes.STRING, 
-        respuesta: DataTypes.STRING,});
+respuesta: DataTypes.STRING,})};
