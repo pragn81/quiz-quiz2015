4b849da043dde37fc9a7d3e9ee79fa4931096f47
pragn81 <pragn81@gmail.com>
2015-08-08 20:10:30 +0100

    Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,27 +1,38 @@
 var models = require ('../models/models.js');
 
+//Autoload - factoriza el codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if (quiz){
+				req.quiz = quiz;
+				next();
+			}
+			else {
+				next(new Error('No existe quizId='+quizId));
+			}
+		}).catch(function (error){next(error);});
+};
+
 //get quizes
 exports.index = function (req, res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index.ejs', {quizes: quizes});	
-	})
+		res.render('quizes/index', {quizes: quizes});	
+	}).catch(function (error){next(error);});
 };
 
 //get quizes/:id
 exports.show = function (req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: quiz});	
+		res.render('quizes/show', {quiz: req.quiz});	
 	})  
 };
 
 //get quizes/:id/answer
 exports.answer = function(req, res) {
-	models.Quiz.find(req.params.quizId).then(function(quiz){
-	    if (req.query.respuesta.toLowerCase()== quiz.respuesta.toLowerCase()){
-	        res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta.toLowerCase()== req.quiz.respuesta.toLowerCase()){
+	        resultado: 'Correcto';
 	    }
-	    else {
-	        res.render('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
-	    }
-	})    
-}; 
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
