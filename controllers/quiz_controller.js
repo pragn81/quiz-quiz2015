3af7383227db70c5df2a3f67db84b2fde2f96333
pragn81 <pragn81@gmail.com>
2015-08-10 21:23:14 +0100

    Ejercicio mod 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -62,7 +62,7 @@ exports.create = function(req, res) {
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				quiz 
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta","respuesta","tema"]})
 				.then( function(){ res.redirect('/quizes')}) 
     		}
   		});
@@ -98,6 +98,7 @@ exports.edit = function (req,res){
 exports.update = function (req,res){
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 	req.quiz
 	.validate()
 	.then(function(err){
@@ -105,7 +106,7 @@ exports.update = function (req,res){
 			res.render('quizes/edit', {quiz:req.quiz, errors: err.errors});
 		} else {
 			req.quiz
-			.save({fields: ["pregunta","respuesta"]})
+			.save({fields: ["pregunta","respuesta","tema"]})
 			.then(function (){res.redirect('/quizes')});
 		}
 	})
