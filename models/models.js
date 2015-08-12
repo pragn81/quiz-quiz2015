0a2aae365cfd874ac9eb9c0b5f8899e050438e08
pragn81 <pragn81@gmail.com>
2015-08-12 19:40:21 +0100

    Añadir comentario

--- models/models.js
+++ models/models.js
@@ -25,13 +25,22 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     omitNull: true      // solo Postgres
   }      
 );
- 
+
 // Importar definicion de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importar definicion de la tabla Comment en comment.js
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz); //cardinalidad 1
+Quiz.hasMany(Comment);  //cardinalidad N
+
 // exportar tablas
 exports.Quiz = Quiz; 
+exports.Comment = Comment; 
+
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
