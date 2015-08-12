8f78202c3ef912e3d51eb9c801e4f7ac65d23237
pragn81 <pragn81@gmail.com>
2015-08-12 20:18:01 +0100

    Moderacion y borrado de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,9 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 
-// Autoload de comandos con :quizId
+// Autoload de comandos con :quizId y commentId
 router.param('quizId',quizController.load);
+router.param('commentId',commentController.load); 
 
 //Definicion de rutas de sesion
 router.get('/login',sessionController.new);
@@ -23,14 +24,18 @@ router.get('/logout',sessionController.destroy);
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
-router.get('/quizes/new', 					quizController.new);
-router.post('/quizes/create', 				quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
-router.put('/quizes/:quizId(\\d+)', 		quizController.update);
-router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
+router.get('/quizes/new', 					sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', 				sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
+//definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',	sessionController.loginRequired, commentController.publish);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/unpublish',	sessionController.loginRequired, commentController.unpublish);
+router.delete('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)',	sessionController.loginRequired, commentController.destroy);
 
 router.get('/author', 						creditsController.credits);
 
