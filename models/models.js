e8957211aedd67ef4bd5450e6c847961bc335d3f
pragn81 <pragn81@gmail.com>
2015-08-12 19:12:09 +0100

    correccion

--- models/models.js
+++ models/models.js
@@ -25,7 +25,7 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     omitNull: true      // solo Postgres
   }      
 );
-
+ 
 // Importar definicion de la tabla Quiz en quiz.js
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
