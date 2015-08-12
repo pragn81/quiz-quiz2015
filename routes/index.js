b59a70933689e91dcac67721d712d2a14a8f7abf
pragn81 <pragn81@gmail.com>
2015-08-12 20:01:23 +0100

    Autenticacion

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var creditsController = require('../controllers/credits_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -12,6 +13,11 @@ router.get('/', function (req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId',quizController.load);
+
+//Definicion de rutas de sesion
+router.get('/login',sessionController.new);
+router.post('/login',sessionController.create);
+router.get('/logout',sessionController.destroy);
 
 //definicion de rutas de /quizes
 router.get('/quizes', 						quizController.index);
